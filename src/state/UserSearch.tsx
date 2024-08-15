2269fda6eae7d33762353fddb7701a1fd3d98d87
KotenkoNata <kotenkonata8@gmail.com>
2024-08-15 16:28:51 +0200

    feat: Type Unions in State

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -8,15 +8,21 @@ const users = [
 
 const UserSearch:React.FC = () => {
     const [name, setName] = useState("");
+    const [user, setUser] = useState<{name: string, age: number} | undefined>();
 
     const onClick = ()=>{
-
+        const foundUser = users.find((user) => user.name === name);
+        setUser(foundUser);
     }
 
     return <div>
-        UserSearch
+        <h3>UserSearch</h3>
         <input value={name} onChange={(e)=>setName(e.target.value)}/>
         <button onClick={onClick}>Find User</button>
+        <div>
+            {user && user.name}
+            {user && user.age}
+        </div>
     </div>
 }
 
