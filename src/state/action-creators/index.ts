41dcbd67447dcde9e943ae185d353c73960970dc
KotenkoNata <kotenkonata8@gmail.com>
2024-08-16 16:17:51 +0200

    feat: Applying Typings to Dispatch

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 import {ActionType} from "../action-types";
 import {Action} from "../actions";
+import {Dispatch} from "redux";
 
 export const searchRepositories = (term: string)=>{
-    return async (dispatch: any)=>{
+    return async (dispatch: Dispatch<Action>)=>{
         dispatch({
             type:ActionType.SEARCH_REPOSITORIES,
         });
@@ -24,7 +25,7 @@ export const searchRepositories = (term: string)=>{
                 payload: names
             })
 
-        }catch (err){
+        } catch (err) {
             if(err instanceof Error){
                 dispatch({
                     type: ActionType.SEARCH_REPOSITORIES_ERROR,
