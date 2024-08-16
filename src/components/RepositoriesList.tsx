5b93d5fecb1b4d775c646bc145eb67186105ac1b
KotenkoNata <kotenkonata8@gmail.com>
2024-08-16 16:33:49 +0200

    feat: Wiring Up to React

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RepositoriesList: React.FC = () => {
+    return <div>
+        <form>
+            <input/>
+            <button type="submit">Search</button>
+        </form>
+    </div>
+}
+
+export default RepositoriesList;
