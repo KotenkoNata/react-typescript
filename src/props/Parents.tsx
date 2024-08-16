f07cd9986c9dcf7d2cd23c96af081a1f90e97191
KotenkoNata <kotenkonata8@gmail.com>
2024-08-16 15:41:14 +0200

    feat: Reducer Setup

--- src/props/Parents.tsx
+++
@@ -1,9 +0,0 @@
-import {ChildAsFC } from "./Child";
-
-const Parents = () =>{
-    return <ChildAsFC color='red' onClick={()=>{console.log('Clicked')}}>
-        sfksdfj
-    </ChildAsFC>
-};
-
-export default Parents;
